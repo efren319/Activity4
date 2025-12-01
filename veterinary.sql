8c4ed2e7dd9e8a88fd5ef01b1d9968ee90835c53
Efren319 <24-33319@g.batstate-u.edu.ph>
2025-12-01 18:55:07 +0800

    feat/remove-appointment-simba

--- veterinary.sql
+++ veterinary.sql
@@ -152,3 +152,6 @@ ADD COLUMN registereddate DATE;
 
 ALTER TABLE invoices
 RENAME COLUMN paymentdate TO paymenttime;
+
+DELETE FROM appointments
+WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
